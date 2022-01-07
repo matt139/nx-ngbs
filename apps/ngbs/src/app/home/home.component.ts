6bcb262eb2e206e84f76e13552cd2e97c7f0c7d1
matt <git@matttelliott.com>
2022-01-07 15:58:34 -0500

    refactor(app): extract home component html

--- apps/ngbs/src/app/home/home.component.ts
+++ apps/ngbs/src/app/home/home.component.ts
@@ -1,27 +1,9 @@
+import { ChangeDetectionStrategy } from '@angular/compiler';
 import { Component } from '@angular/core'
 
 @Component({
   selector: 'ngbs-root',
-  template: `
-    <h1>NGBS</h1>
-    <h2>A collection of Angular modules based on Bootstrap</h2>
-
-    <ul>
-      <li>
-        <a routerLink="/auth"> Auth </a>
-        <ul>
-          <li>
-            <a routerLink="/auth/sign-up">sign up</a>
-          </li>
-          <li>
-            <a routerLink="/auth/log-in">log in</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a routerLink="/utils">Utils</a>
-      </li>
-    </ul>
-  `,
+  templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {}
