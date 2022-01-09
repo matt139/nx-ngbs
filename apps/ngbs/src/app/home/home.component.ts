eb379b88d04a4f29b6c67b3ef162d2c1eb48e8c6
matt <git@matttelliott.com>
2022-01-09 14:24:18 -0500

    feat(home): add auth/settings to home

--- apps/ngbs/src/app/home/home.component.ts
+++ apps/ngbs/src/app/home/home.component.ts
@@ -3,6 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
 @Component({
   selector: 'ngbs-root',
   templateUrl: './home.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {}
